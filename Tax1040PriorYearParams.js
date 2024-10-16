688ff843a22e500b10b807b30810e202997982a8
tttttttttttaq <135803762+tttttttttttaq@users.noreply.github.com>
2024-10-15 22:20:26 -0400

    Add files via upload

---
+++ Tax1040PriorYearParams.js
@@ -0,0 +1,76 @@
+
+
+
+    KJE.parameters.set("EDUCATOR_EXPENSES",0);
+    KJE.parameters.set("LINE_10_TAXABLE_REFUNDS_OR_CREDITS_OF_STATE_AND_LOCAL_INCOME_TAXES",0);
+    KJE.parameters.set("LINE_11_ALIMONY_RECEIVED",0);
+    KJE.parameters.set("LINE_12_BUSINESS_INCOME_OR_LOSS",0);
+    KJE.parameters.set("LINE_12_SPOUSE_BUSINESS_INCOME_OR_LOSS",0);
+    KJE.parameters.set("LINE_13_CAPITAL_GAIN_OR_LOSS",0);
+    KJE.parameters.set("LINE_14_OTHER_GAINS_OR_LOSSES",0);
+    KJE.parameters.set("LINE_15_TAXABLE_IRA_DISTRIBUTIONS",0);
+    KJE.parameters.set("LINE_16_TAXABLE_PENSIONS_AND_ANNUITY_DISTRIBUTIONS",0);
+    KJE.parameters.set("LINE_17_INCOME_FROM_SCHEDULE_E",0);
+    KJE.parameters.set("LINE_17_INCOME_FROM_SCHEDULE_E2",0);
+    KJE.parameters.set("LINE_18_FARM_INCOME_OR_LOSS",0);
+    KJE.parameters.set("LINE_19_UNEMPLOYMENT_COMPENSATION",0);
+    KJE.parameters.set("LINE_1_FILING_STATUS",KJE.Default.iSGL);
+    KJE.parameters.set("LINE_20a_SOCIAL_SECURITY_BENEFITS",0);
+    KJE.parameters.set("LINE_21_OTHER_INCOME",0);
+    KJE.parameters.set("LINE_23_IRA_DEDUCTION",0);
+    KJE.parameters.set("LINE_24_STUDENT_LOAN_INTEREST_DEDUCTION",0);
+    KJE.parameters.set("LINE_26_MOVING_EXPENSES",0);
+    KJE.parameters.set("LINE_27_ONEHALF_OF_SELFEMPLOYMENT_TAX",0);
+    KJE.parameters.set("LINE_28_SELFEMPLOYED_HEALTH_INSURANCE_DEDUCTION",0);
+    KJE.parameters.set("LINE_29_KEOP_AND_SELF_EMPLOYED_SEP_AND_SIMPLE_PLANS",0);
+    KJE.parameters.set("LINE_30_PENALTY_ON_EARLY_WITHDRAWAL_OF_SAVINGS",0);
+    KJE.parameters.set("LINE_31_ALIMONY_PAID",0);
+    KJE.parameters.set("ITEMIZED_MEDICAL_EXPENSES",0);
+    KJE.parameters.set("ITEMIZED_STATE_LOCAL_TAXES",0);
+    KJE.parameters.set("ITEMIZED_INTEREST_PAID",0);
+    KJE.parameters.set("ITEMIZED_GIFTS_TO_CHARITY_CASH",0);
+    KJE.parameters.set("ITEMIZED_GIFTS_TO_CHARITY_NONCASH",0);
+    KJE.parameters.set("LINE_41_CREDIT_FOR_CHILD_AND_DEPENDENT_CARE_EXPENSES",0);
+    KJE.parameters.set("LINE_42_CREDIT_FOR_THE_ELDERLY_OR_DISABLED",0);
+    KJE.parameters.set("LINE_44_EDUCATION_CREDITS",0);
+    KJE.parameters.set("LINE_45_RESIDENTIAL_ENERGY",0);
+    KJE.parameters.set("LINE_46_FOREIGN_TAX_CREDIT",0);
+    KJE.parameters.set("LINE_47_OTHER_CREDITS",0);
+    KJE.parameters.set("LINE_50_SELFEMPLOYMENT_TAX",0);
+    KJE.parameters.set("AMTI_OTHER_AJUSTMENTS",0);
+    KJE.parameters.set("LINE_51a_EXCESS_PREMIMUM_TAX",0);
+    KJE.parameters.set("LINE_52_SOCIAL_SECURITY_AND_MEDICARE_ON_TIPS_NOT_REPORTED",0);
+    KJE.parameters.set("LINE_53_TAX_ON_IRAS_AND_OTHER_RETIREMENT_PLANS",0);
+    KJE.parameters.set("LINE_54_ADVANCE_EARNED_INCOME_CREDIT_PAYMENTS",0);
+    KJE.parameters.set("LINE_55_HOUSEHOLD_EMPLOYMENT_TAXES",0);
+    KJE.parameters.set("LINE_57_FEDERAL_INCOME_TAX_WITHHELD",0);
+    KJE.parameters.set("LINE_58_2000_ESTIMATED_TAX_PAYMENTS",0);
+    KJE.parameters.set("LINE_59_EARNED_INCOME_CREDIT",0);
+    KJE.parameters.set("LINE_61_AMOUNT_PAID_WITH_FORM_4868",0);
+    KJE.parameters.set("LINE_62_EXCESS_SOCIAL_SECURITY_AND_RRTA_TAX_WITHHELD",0);
+    KJE.parameters.set("LINE_63_OTHER_PAYMENTS",0);
+    KJE.parameters.set("LINE_6A_PERSONAL_EXEMPTION",1);
+    KJE.parameters.set("LINE_6B_SPOUSE_EXEMPTION",1);
+    KJE.parameters.set("LINE_6C_DEPENDENTS",0);
+    KJE.parameters.set("LINE_6C_DEPENDENTS_QUALIFYING_FOR_CHILD_TAX_CREDIT",0);
+    KJE.parameters.set("LINE_6C_DEPENDENTS_QUALIFYING_FOR_CHILD_TAX_CREDIT_UNDER6",0);
+    KJE.parameters.set("LINE_7_WAGES_SALARIES_TIPS_ETC",0);
+    KJE.parameters.set("LINE_7_SPOUSE_WAGES_SALARIES_TIPS_ETC",0);
+    KJE.parameters.set("LINE_8A_TAXABLE_INTEREST",0);
+    KJE.parameters.set("LINE_8B_TAXEXEMPT_INTEREST",0);
+    KJE.parameters.set("LINE_9_ORDINARY_DIVIDENDS",0);
+
+
+    KJE.parameters.set("EITHER_SPOUSE_AGE_QUALIFY",false);
+    KJE.Scroll=false;
+    
+
+/**V3_CUSTOM_CODE**/
+/* <!--
+  Financial Calculators, &copy;1998-2024 KJE Computer Solutions, Inc.
+  For more information please see:
+  <A HREF="https://www.dinkytown.net">https://www.dinkytown.net</A>
+ -->
+ */
+
+
